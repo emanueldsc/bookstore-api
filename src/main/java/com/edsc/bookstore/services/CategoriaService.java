086212def8a41260a3d1fb9476b10c1c989ca135
Emanuel Douglas <emanuel.douglas.sc@gmail.com>
2022-07-13 12:17:16 -0300

    UPDATE for category.

--- src/main/java/com/edsc/bookstore/services/CategoriaService.java
+++ src/main/java/com/edsc/bookstore/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.edsc.bookstore.domain.Categoria;
+import com.edsc.bookstore.dto.CategoriaDTO;
 import com.edsc.bookstore.repositories.CategoryaRepository;
 import com.edsc.bookstore.services.exceptions.ObjectNotFoundException;
 
@@ -29,6 +30,13 @@ public class CategoriaService {
     public Categoria create(Categoria obj) {
         obj.setId(null);
         return this.repository.save(obj);
+    }
+
+    public Categoria update(Integer id, CategoriaDTO objDto) {
+        Categoria obj = findById(id);
+        obj.setNome(objDto.getNome());
+        obj.setDescricao(objDto.getDescricao());
+        return repository.save(obj);
     }
 
 }
