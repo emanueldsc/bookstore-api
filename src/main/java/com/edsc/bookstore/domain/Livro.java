ccabf0831f960ff1c9c163424696930f6a3cc624
Emanuel Douglas <emanuel.douglas.sc@gmail.com>
2022-07-12 22:35:09 -0300

    automatically stating database.

--- src/main/java/com/edsc/bookstore/domain/Livro.java
+++ src/main/java/com/edsc/bookstore/domain/Livro.java
@@ -3,16 +3,17 @@ package com.edsc.bookstore.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+@Entity
 public class Livro implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
