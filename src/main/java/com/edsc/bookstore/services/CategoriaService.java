2e1d1b8a36d438ea7db81dc950c31b0f5ec953b1
Emanuel Douglas <emanuel.douglas.sc@gmail.com>
2022-07-13 10:47:44 -0300

    Custom exception for category.

--- src/main/java/com/edsc/bookstore/services/CategoriaService.java
+++ src/main/java/com/edsc/bookstore/services/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.edsc.bookstore.domain.Categoria;
 import com.edsc.bookstore.repositories.CategoryaRepository;
+import com.edsc.bookstore.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-    
+
     @Autowired
     private CategoryaRepository categoryaRepository;
 
     public Categoria findById(Integer id) {
         Optional<Categoria> opt = this.categoryaRepository.findById(id);
-        return opt.orElse(null);
+        return opt.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto Não encontrado Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 
 }
